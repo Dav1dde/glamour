f26bf6d6073111e4dc981829aae0406205cafa0b
David Herberth <admin@dav1d.de>
2012-02-03 17:03:40 +0100

    added util.d and worked on texture.d

--- glamour/texture.d
+++ glamour/texture.d
@@ -1,14 +1,176 @@
 module glamour.texture;
 
 private {
+    import derelict.opengl.gl : GLenum, GLuint, GLint, GLsizei,
+                                glGenTextures, glBindTexture, glActiveTexture,
+                                glTexImage1D, glTexImage2D, glTexParameteri, glTexParameterf,
+                                glGetTexParameterfv, GL_TEXTURE0,
+                                GL_TEXTURE_1D, GL_TEXTURE_2D, GL_TEXTURE_BORDER_COLOR;
+    import derelict.opengl.glext : glGenerateMipmap;
+    import derelict.devil.il : ILuint, ilGenImages, ilBindImage, ilLoadImage, ilConvertImage,
+                               ilGetData, ilGetInteger, IL_RGB, IL_UNSIGNED_BYTE,
+                                IL_IMAGE_FORMAT, IL_IMAGE_TYPE, IL_IMAGE_WIDTH, IL_IMAGE_HEIGHT;
+    import glamour.util : glenum2size;
+    import std.traits : isPointer;
+    import std.string : toStringz;
     
+    debug {
+        import std.stdio : writefln;
+    }
 }
 
 
-struct Texture {
+class TextureError : Exception {
+    this(string msg) {
+        super(msg);
+    }
+}
+
+
+mixin template CommonTextureMethods() {
+    void set_paramter(T)(GLenum name, T params) if(is(T : int) || is(T : float)) {
+        static if(is(T : int)) {
+            glTexParameteri(target, name, params);
+        } else {
+            glTexParameterf(target, name, params);
+        }
+    }
+    
+    float[] get_parameter(GLenum name) {
+        float[] ret;
+        
+        if(name == GL_TEXTURE_BORDER_COLOR) {
+            ret.length = 4;
+        } else {
+            ret.length = 1;
+        }
+        
+        glGetTexParameterfv(target, name, ret.ptr);
+        return ret;
+    }
+    
+    void bind() {
+        glActiveTexture(unit);
+        glBindTexture(target, texture);
+    }
+    
+    void unbind() {
+        glBindTexture(GL_TEXTURE_1D, 0);
+    }
+}
+
+
+struct Texture1D {
+    static const GLenum target = GL_TEXTURE_1D;
+    
     GLuint texture;
     alias texture this;
     
-    this() {
+    GLint internal_format;
+    GLenum format;
+    GLenum type;
+    GLenum unit;
+       
+    mixin CommonTextureMethods;
+
+    this()(GLint internal_format_, GLenum format_, GLenum type_, GLenum unit_=GL_TEXTURE0) {
+        internal_format = internal_format_;
+        format = format_;
+        type = type_;
+        unit = unit_;
+        
+        glGenTextures(1, &texture);
+    }
+       
+    this(T)(T data, GLint internal_format, GLenum format, GLenum type, GLenum unit=GL_TEXTURE0) {
+        this(internal_format, format, type, unit);
+        
+        set_data(data);
+    }
+    
+    void set_data(T)(T data) {
+        bind();
+        glTexImage1D(GL_TEXTURE_1D, 0, internal_format, cast(int)(data.length), 0, format, type, data.ptr);
+        unbind();
+        
+        Texture1D(data, 1, 1, 1);
+    }
+
+}
+
+struct Texture2D {
+    static const GLenum target = GL_TEXTURE_2D;
+    
+    GLuint texture;
+    alias texture this;
+    
+    GLint internal_format;
+    GLsizei width;
+    GLsizei height;
+    GLenum format;
+    GLenum type;
+    GLenum unit;
+    bool mipmaps = true;
+    
+    mixin CommonTextureMethods;
+
+    // ugly workaround, "Error: constructor call must be in a constructor"?
+    private void ctor(GLint internal_format_, GLsizei width_, GLsizei height_,
+                      GLenum format_, GLenum type_, GLenum unit_=GL_TEXTURE0, bool mipmaps_=true) {
+        internal_format = internal_format_;
+        width = width_;
+        height = height_;
+        format = format_;
+        type = type_;
+        unit = unit_;
+        mipmaps = mipmaps_;
+        
+        glGenTextures(1, &texture);
+    }
+        
+    this()(GLint internal_format, GLsizei width, GLsizei height,
+           GLenum format, GLenum type, GLenum unit=GL_TEXTURE0, bool mipmaps=true) {
+        ctor(internal_format, width, height, format, type, unit, mipmaps);
+    }
+    
+    this(T)(T data, GLint internal_format, GLsizei width, GLsizei height,
+            GLenum format, GLenum type, GLenum unit=GL_TEXTURE0, bool mipmaps=true) {
+        ctor(internal_format, width, height, format, type, unit, mipmaps);
+
+        set_data(data);
+    }
+    
+    void set_data(T)(T data, GLint level=0) {
+        bind();
+        
+        static if(isPointer!T) {
+            auto d = data;
+        } else {
+            auto d = data.ptr;
+        }
+        
+        glTexImage2D(GL_TEXTURE_2D, level, internal_format, width, height, 0, format, type, d);
+        if(mipmaps) {
+            glGenerateMipmap(GL_TEXTURE_2D);
+        }
+        
+        unbind();
+    }
+    
+    static Texture2D from_image(string filename) {
+        debug {
+            writefln("using Texture2D.from_image, DevIL loaded and initialized?");
+        }
+        ILuint id;
+        ilGenImages(1, &id);
+        
+        if(!ilLoadImage(toStringz(filename))) {
+            throw new TextureError("Unable to load image: " ~ filename);
+        }
+        
+        ilConvertImage(IL_RGB, IL_UNSIGNED_BYTE);
+        
+        return Texture2D(ilGetData(), ilGetInteger(IL_IMAGE_FORMAT), ilGetInteger(IL_IMAGE_WIDTH),
+                         ilGetInteger(IL_IMAGE_HEIGHT), ilGetInteger(IL_IMAGE_FORMAT), ilGetInteger(IL_IMAGE_TYPE));        
     }
 }
---
+++ glamour/util.d
@@ -0,0 +1,106 @@
+module glamour.util;
+
+private {
+    import derelict.opengl.gl : GL_BYTE, GL_UNSIGNED_BYTE, GL_SHORT, GL_UNSIGNED_SHORT,
+                                GL_INT, GL_UNSIGNED_INT, GL_FLOAT, GL_DOUBLE, GLenum;
+}
+
+template type2glenum(T) {
+    static if(is(T == byte)) {
+        GLenum type2glenum = GL_BYTE;
+    } else static if(is(T == ubyte)) {
+        GLenum type2glenum = GL_UNSIGNED_BYTE;
+    } else static if(is(T == short)) {
+        GLenum type2glenum = GL_SHORT;
+    } else static if(is(T == ushort)) {
+        GLenum type2glenum = GL_UNSIGNED_SHORT;
+    } else static if(is(T == int)) {
+        GLenum type2glenum = GL_INT;
+    } else static if(is(T == uint)) {
+        GLenum type2glenum = GL_UNSIGNED_INT;
+    } else static if(is(T == float)) {
+        GLenum type2glenum = GL_FLOAT;
+    } else static if(is(T == double)) {
+        GLenum type2glenum = GL_DOUBLE;
+    } else {
+        static assert(false, T.stringof ~ " cannot be represented as GLenum");
+    }
+}
+
+template glenum2type(GLenum t) {
+    static if(t == GL_BYTE) {
+        alias byte glenum2type;
+    } else static if(t == GL_UNSIGNED_BYTE) {
+        alias ubyte glenum2type;
+    } else static if(t == GL_SHORT) {
+        alias short glenum2type;
+    } else static if(t == GL_UNSIGNED_SHORT) {
+        alias ushort glenum2type;
+    } else static if(t == GL_INT) {
+        alias int glenum2type;
+    } else static if(t == GL_UNSIGNED_INT) {
+        alias uint glenum2type;
+    } else static if(t == GL_FLOAT) {
+        alias float glenum2type;
+    } else static if(t == GL_DOUBLE) {
+        alias double glenum2type;
+    } else {
+        static assert(false, T.stringof ~ " cannot be represented as D-Type");
+    }
+}
+
+unittest {
+    assert(GL_BYTE == type2glenum!byte);
+    assert(GL_UNSIGNED_BYTE == type2glenum!ubyte);
+    assert(GL_SHORT == type2glenum!short);
+    assert(GL_UNSIGNED_SHORT == type2glenum!ushort);
+    assert(GL_INT == type2glenum!int);
+    assert(GL_UNSIGNED_INT == type2glenum!uint);
+    assert(GL_FLOAT == type2glenum!float);
+    assert(GL_DOUBLE == type2glenum!double);
+    
+    assert(is(byte : glenum2type!GL_BYTE));
+    assert(is(ubyte : glenum2type!GL_UNSIGNED_BYTE));
+    assert(is(short : glenum2type!GL_SHORT));
+    assert(is(ushort : glenum2type!GL_UNSIGNED_SHORT));
+    assert(is(int : glenum2type!GL_INT));
+    assert(is(uint : glenum2type!GL_UNSIGNED_INT));
+    assert(is(float : glenum2type!GL_FLOAT));
+    assert(is(double : glenum2type!GL_DOUBLE));
+}
+
+template glenum2sizect(GLenum t) {
+    static if(t == GL_BYTE) {
+        enum glenum2sizect = byte.sizeof;
+    } else static if(t == GL_UNSIGNED_BYTE) {
+        enum glenum2sizect = ubyte.sizeof;
+    } else static if(t == GL_SHORT) {
+        enum glenum2sizect = short.sizeof;
+    } else static if(t == GL_UNSIGNED_SHORT) {
+        enum glenum2sizect = ushort.sizeof;
+    } else static if(t == GL_INT) {
+        enum glenum2sizect = int.sizeof;
+    } else static if(t == GL_UNSIGNED_INT) {
+        enum glenum2sizect = uint.sizeof;
+    } else static if(t == GL_FLOAT) {
+        enum glenum2sizect = float.sizeof;
+    } else static if(t == GL_DOUBLE) {
+        enum glenum2sizect = double.sizeof;
+    } else {
+        static assert(false, T.stringof ~ " cannot be represented as D-Type");
+    }
+}
+
+int glenum2size(GLenum t) {
+    switch(t) {
+        case GL_BYTE: return glenum2sizect!GL_BYTE;
+        case GL_UNSIGNED_BYTE: return glenum2sizect!GL_UNSIGNED_BYTE;
+        case GL_SHORT: return glenum2sizect!GL_SHORT;
+        case GL_UNSIGNED_SHORT: return glenum2sizect!GL_UNSIGNED_SHORT;
+        case GL_INT: return glenum2sizect!GL_INT;
+        case GL_UNSIGNED_INT: return glenum2sizect!GL_UNSIGNED_INT;
+        case GL_FLOAT: return glenum2sizect!GL_FLOAT;
+        case GL_DOUBLE: return glenum2sizect!GL_DOUBLE;
+        default: throw new Exception("Unknown GLenum");
+    }
+}
