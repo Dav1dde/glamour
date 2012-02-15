ffaa8e6963854d3ac91f3f083c6a91746948c9c8
David Herberth <admin@dav1d.de>
2012-02-15 18:49:11 +0100

    added gl.d

---
+++ glamour/gl.d
@@ -0,0 +1,14 @@
+/**
+If compiled with version=Derelict3 glamour will use Derelict3 as OpenGL backend, otherwise Derelict2.
+If glamour gets compiled with Derelict3, you can not load textures from images with DevIL!
+*/
+
+
+module glamour.gl;
+
+version(Derelict3) {
+    public import derelict.opengl3.gl3;
+} else {
+    public import derelict.opengl.gl;
+    public import derelict.opengl.glext;
+}
