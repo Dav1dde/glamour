79c5098bf3bf589ab177e4b27ccd3dd0a1a2e4fb
David <admin@dav1d.de>
2012-06-25 20:45:04 +0200

    improved vbos

--- glamour/vbo.d
+++ glamour/vbo.d
@@ -9,21 +9,6 @@ private {
                         glGenBuffers, glDeleteBuffers;
 }
 
-/// Every Element*-Struct will mixin this template.
-mixin template BufferData() {
-    /// Stores the OpenGL type of the passed data (e.g. GL_FLOAT).
-    GLenum type;
-    /// Specifies the expected usage pattern of the data store.
-    GLenum hint;
-    /// Length of the passed data, note it's the length of a void[] array.
-    size_t length = 0;
-
-    private void set_buffer_data(GLenum t, GLenum h) {
-        type = t;
-        hint = h;
-    }
-}
-
 /// Interface every buffer implements
 interface IBuffer {
     void bind(); /// 
@@ -34,12 +19,15 @@ interface IBuffer {
 /// Represents an OpenGL element buffer.
 /// The constructor must be used to avoid segmentation faults.
 class ElementBuffer : IBuffer {
-    mixin BufferData;
-    
     /// The OpenGL buffer name.
     GLuint buffer;
     /// Alias this to buffer.
     alias buffer this;
+
+    /// Specifies the expected usage pattern of the data store.
+    GLenum hint;
+    /// Length of the passed data, note it's the length of a void[] array.
+    size_t length = 0;
     
     /// Initializes the buffer.
     this() {
@@ -74,7 +62,7 @@ class ElementBuffer : IBuffer {
         glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0); //or unbind()
         
         length = data.length;
-        set_buffer_data(type, hint);
+        this.hint = hint;
     }
 }
 
@@ -82,12 +70,15 @@ class ElementBuffer : IBuffer {
 /// Represents an OpenGL buffer.
 /// The constructor must be used to avoid segmentation faults.
 class Buffer : IBuffer {
-    mixin BufferData;
-    
     /// The OpenGL buffer name.
     GLuint buffer;
     /// Alias this to buffer.
     alias buffer this;
+
+    /// Specifies the expected usage pattern of the data store.
+    GLenum hint;
+    /// Length of the passed data, note it's the length of a void[] array.
+    size_t length = 0;
     
     // Initializes the buffer.
     this() {
@@ -99,9 +90,9 @@ class Buffer : IBuffer {
     /// data = any kind of data.
     /// type = OpenGL type of the data (e.g. GL_FLOAT)
     /// hint = Specifies the expected usage pattern of the data store.
-    this(void[] data, GLenum type, GLenum hint = GL_STATIC_DRAW) {
+    this(void[] data, GLenum hint = GL_STATIC_DRAW) {
         glGenBuffers(1, &buffer);
-        set_data(data, type, hint);
+        set_data(data, hint);
     }
     
     /// Deletes the buffer.
@@ -117,7 +108,7 @@ class Buffer : IBuffer {
     /// Binds the buffer and sets the vertex attrib pointer.
     /// size = Specifies the number of components per generic vertex attribute.
     /// stride = Specifies the byte offset between consecutive generic vertex attributes.
-    void bind(GLuint attrib_location, GLint size=4, GLsizei offset=0, GLsizei stride=0) {
+    void bind(GLuint attrib_location, GLenum type, GLint size=4, GLsizei offset=0, GLsizei stride=0) {
         glBindBuffer(GL_ARRAY_BUFFER, buffer);
         glEnableVertexAttribArray(attrib_location);
         glVertexAttribPointer(attrib_location, size, type, GL_FALSE, stride, cast(void *)offset);
@@ -129,13 +120,13 @@ class Buffer : IBuffer {
     }
     
     /// Uploads data to the GPU.
-    void set_data(void[] data, GLenum type, GLenum hint = GL_STATIC_DRAW) {
+    void set_data(void[] data, GLenum hint = GL_STATIC_DRAW) {
         glBindBuffer(GL_ARRAY_BUFFER, buffer); // or bind()
         glBufferData(GL_ARRAY_BUFFER, data.length, data.ptr, hint);
         glBindBuffer(GL_ARRAY_BUFFER, 0); //or unbind()
     
         length = data.length;
-        set_buffer_data(type, hint);
+        this.hint = hint;
     }
     
     /// Updates the Buffer, using glBufferSubData.
