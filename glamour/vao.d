45014f24d5bff41a25d0679e6ec1f475b701552b
David <admin@dav1d.de>
2013-06-16 12:52:31 +0200

    Added set_attrib_pointer to vao

--- glamour/vao.d
+++ glamour/vao.d
@@ -5,6 +5,7 @@ private {
                         glEnableVertexAttribArray, glDisableVertexAttribArray,
                         glDeleteVertexArrays,
                         GLuint, GLenum, GLint, GLsizei, GLboolean, GL_FALSE;
+    import glamour.shader : Shader;
     import glamour.util : checkgl;
 
     debug {
@@ -47,5 +48,26 @@ class VAO : IVAO {
     void remove() {
         checkgl!glDeleteVertexArrays(1, &vao);
         vao = 0;
+    }
+
+    /// Binds the VAO and sets the vertex attrib pointer.
+    /// Params:
+    /// type = Specifies the data type of each component in the array.
+    /// size = Specifies the number of components per generic vertex attribute.
+    /// offset = Specifies a offset of the first component of the first generic vertex attribute in the array in the data store of the buffer.
+    /// stride = Specifies the byte offset between consecutive generic vertex attributes.
+    /// normalized = Specifies whether fixed-point data values should be normalized (GL_TRUE) or
+    ///                converted directly as fixed-point values (GL_FALSE = default) when they are accessed.
+    void set_attrib_pointer(GLuint attrib_location, GLenum type, GLint size, GLsizei offset,
+              GLsizei stride, GLboolean normalized=GL_FALSE) {
+        checkgl!glBindVertexArray(vao);
+        checkgl!glEnableVertexAttribArray(attrib_location);
+        checkgl!glVertexAttribPointer(attrib_location, size, type, normalized, stride, cast(void *)offset);
+    }
+
+    /// ditto
+    void set_attrib_pointer(Shader shader, string location, GLenum type, GLint size, GLsizei offset,
+              GLsizei stride, GLboolean normalized=GL_FALSE) {
+        set_attrib_pointer(shader.get_attrib_location(location), type, size, offset, stride, normalized);
     }
 }
