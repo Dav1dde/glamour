81f5fa7968f9280c243d823badb1782e7fdbce3b
David <admin@dav1d.de>
2013-08-01 15:45:35 +0200

    add glad to glamour/gl

--- glamour/gl.d
+++ glamour/gl.d
@@ -8,6 +8,8 @@ module glamour.gl;
 
 version(Derelict3) {
     public import derelict.opengl3.gl3;
+} else version(glad) {
+    public import glad.gl;
 } else {
     public import derelict.opengl.gl;
     public import derelict.opengl.glext;
