400ff8e117d7bd5d38ba868d4ea3bb16d0c31152
David <admin@dav1d.de>
2014-03-03 18:23:27 +0100

    revert last changes, workaround for glad bug

--- glamour/gl.d
+++ glamour/gl.d
@@ -9,7 +9,7 @@ module glamour.gl;
 version(Derelict3) {
     public import derelict.opengl3.gl3;
 } else version(glad) {
-    public import glad.gl.all;
+    public import glad.gl.gl;
 } else {
     public import derelict.opengl.gl;
     public import derelict.opengl.glext;
